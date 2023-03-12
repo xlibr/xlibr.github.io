fd660b3cb189dcf0f892c1032bb77d48c1702f53
xlibr <127614635+xlibr@users.noreply.github.com>
2023-03-13 03:09:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 function test(){
 alert("Hello World!")}
+
+fetch('${{ secrets.DATA_API }}/action/findOne', {
+  method: 'POST',
+  body: JSON.stringify({
+    dataSource: 'Cluster0',
+    database: 'data',
+    collection: 'note',
+    filter: {
+      text: 'note test'
+    }
+  }),
+  headers: {
+    'Content-Type': 'application/json',
+    'api-key': '${{ secrets.API_KEY_0 }}'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
